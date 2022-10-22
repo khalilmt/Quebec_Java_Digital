2abf3fa1943097f61a29b092d2417b3db96aa942
khalilmt <kalilmteixeira@gmail.com>
2022-10-22 20:42:30 -0300

    Novos exercicios

---
+++ controleFluxo/src/ExemploForV1.java
@@ -0,0 +1,11 @@
+public class ExemploForV1 {
+
+    public static void main (String[] args) {
+
+        for ( int carneirinhos = 1; carneirinhos <=20; carneirinhos ++ ) {
+            System.out.println("Contando Carneirinhos" + carneirinhos);
+        }
+
+        System.out.println("Dormiu");
+    }
+}
---
+++ controleFluxo/src/ExemploForV2.java
@@ -0,0 +1,12 @@
+public class ExemploForV2 {
+
+    public static void main (String[] args) {
+
+        String alunos [] = {"FELIPE", "JONAS", "JULIANA", "MARCOS"};
+
+        for (int x = 0; x < alunos.length; x ++){
+
+            System.out.println("O aluno no indice x: " + x + " é " + alunos[ x ]);
+        }
+    }
+}
